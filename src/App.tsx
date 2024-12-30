fbb968219664b61bc7b4896b4a2c4b3b09777f66
yeonhee7935 <yeonhee7935@naver.com>
2024-12-31 00:17:58 +0900

    ✨ Feat: 공통 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
-function App() {
-  return <div>Hello world</div>;
+import { BrowserRouter, RouteObject, useRoutes } from "react-router-dom"
+import { ROUTES } from "./utils/routes"
+import { Layout } from "@/components/ui/layout"
+
+const routes: RouteObject[] = [
+  {
+    path: ROUTES.LOGIN,
+    element: <></>,
+  },
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: ROUTES.COMPLETED_ORDER,
+        element: <></>,
+      },
+      {
+        path: ROUTES.ACTIVE_ORDER,
+        element: <></>,
+      },
+      {
+        path: ROUTES.STORE,
+        element: <></>,
+      },
+      {
+        path: ROUTES.MENU,
+        element: <></>,
+      },
+    ],
+  },
+]
+
+function AppRoutes() {
+  const element = useRoutes(routes)
+  return element
 }
 
-export default App;
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/utils/routes.ts
@@ -0,0 +1,9 @@
+// src/utils/routes.ts
+
+export const ROUTES = {
+  LOGIN: "/login",
+  COMPLETED_ORDER: "/orders/completed",
+  ACTIVE_ORDER: "/orders/active",
+  STORE: "/store",
+  MENU: "/menu",
+}
