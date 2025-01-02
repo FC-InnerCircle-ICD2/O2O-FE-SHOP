c5ee246ed6353452a7a3cecc2a389fc4db4b6c43
yeonhee7935 <yeonhee7935@naver.com>
2025-01-02 22:05:49 +0900

    ♻️ Refactor: 공통레이아웃 header, body, footer로 나누기

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,125 +28,135 @@ import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } f
 import { Outlet } from "react-router-dom"
 import { ROUTES } from "@/utils/routes"
 
+function Header() {
+  return (
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                Select Workspace
+                <ChevronDown className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+              <DropdownMenuItem>
+                <span>Acme Inc</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Acme Corp.</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+  )
+}
+function Body() {
+  const MENU_ITEMS = [
+    {
+      label: "주문",
+      items: [
+        {
+          icon: File,
+          label: "신규,진행중",
+          route: ROUTES.ACTIVE_ORDER,
+          badge: 24,
+        },
+        {
+          icon: FileCheck,
+          label: "완료",
+          route: ROUTES.COMPLETED_ORDER,
+        },
+      ],
+    },
+    {
+      label: "가게",
+      items: [
+        {
+          icon: Store,
+          label: "가게정보",
+          route: ROUTES.STORE_INFO,
+        },
+        {
+          icon: BookText,
+          label: "메뉴",
+          route: ROUTES.STORE_MENU,
+        },
+        {
+          icon: MessageSquareText,
+          label: "리뷰",
+          route: ROUTES.STORE_REVIEW,
+        },
+      ],
+    },
+  ]
+  return (
+    <SidebarContent>
+      {MENU_ITEMS.map(({ label, items }) => (
+        <SidebarGroup key={label}>
+          <SidebarGroupLabel>{label}</SidebarGroupLabel>
+          <SidebarContent>
+            <SidebarGroupContent>
+              {items.map(({ icon: Icon, label, route, badge }) => (
+                <SidebarMenu key={label}>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <a href={route}>
+                        <Icon />
+                        <span>{label}</span>
+                      </a>
+                    </SidebarMenuButton>
+                    {badge && <SidebarMenuBadge>{badge}</SidebarMenuBadge>}
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              ))}
+            </SidebarGroupContent>
+          </SidebarContent>
+        </SidebarGroup>
+      ))}
+    </SidebarContent>
+  )
+}
+function Footer() {
+  return (
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                <User2 /> Username
+                <ChevronUp className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width]">
+              <DropdownMenuItem>
+                <span>Account</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Billing</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <span>Sign out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
+  )
+}
+
 export function Layout() {
   return (
     <SidebarProvider>
       <Sidebar>
-        <SidebarHeader>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuButton>
-                    Select Workspace
-                    <ChevronDown className="ml-auto" />
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-                  <DropdownMenuItem>
-                    <span>Acme Inc</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Acme Corp.</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>주문</SidebarGroupLabel>
-            <SidebarContent>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <a href={ROUTES.ACTIVE_ORDER}>
-                        <File />
-                        <span>신규,진행중</span>
-                      </a>
-                    </SidebarMenuButton>
-                    <SidebarMenuBadge>24</SidebarMenuBadge>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <a href={ROUTES.COMPLETED_ORDER}>
-                        <FileCheck />
-                        <span>완료</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarContent>
-          </SidebarGroup>
-          <SidebarGroup>
-            <SidebarGroupLabel>가게</SidebarGroupLabel>
-            <SidebarContent>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <a href={ROUTES.STORE_INFO}>
-                        <Store />
-                        <span>가게정보</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <a href={ROUTES.STORE_MENU}>
-                        <BookText />
-                        <span>메뉴</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <a href={ROUTES.STORE_REVIEW}>
-                        <MessageSquareText />
-                        <span>리뷰</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarContent>
-          </SidebarGroup>
-          <SidebarGroup />
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuButton>
-                    <User2 /> Username
-                    <ChevronUp className="ml-auto" />
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width]">
-                  <DropdownMenuItem>
-                    <span>Account</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Billing</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Sign out</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
+        <Header />
+        <Body />
+        <Footer />
       </Sidebar>
       <main>
         <SidebarTrigger />
