0e525c893a3124d76b092bc25cacf9c1afcc5961
Suhyeon Jeon <suhyeon.jeon@fig.xyz>
2025-01-11 23:28:26 +0900

    Feat: sidebar active 상태 추가

--- src/components/layout/body.tsx
+++ src/components/layout/body.tsx
@@ -1,91 +1,81 @@
 import { ROUTES } from "@/utils/routes"
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible"
 import { useLocation, useNavigate } from "react-router-dom"
-import Icon, { IconName } from "../Icon"
+import Icon from "../Icon"
 import { SidebarContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar"
 
 export function Body() {
   const navigate = useNavigate()
   const { pathname } = useLocation()
-  console.log(pathname)
-
-  const menuItems: {
-    name: string
-    icon: IconName
-    path?: string
-    subMenu?: { name: string; path: string }[]
-  }[] = [
-    {
-      name: "대시보드",
-      icon: "LayoutDashboard",
-      path: ROUTES.DASHBOARD,
-    },
-    {
-      name: "주문",
-      icon: "ClipboardPen",
-      subMenu: [
-        {
-          name: "신규 • 진행중",
-          path: ROUTES.ACTIVE_ORDER,
-        },
-        {
-          name: "완료",
-          path: ROUTES.COMPLETED_ORDER,
-        },
-      ],
-    },
-  ]
 
   return (
     <SidebarContent className="gap-3">
       <SidebarMenu className="flex flex-col gap-3">
-        {menuItems.map((item) => (
-          <Collapsible defaultOpen className="group/collapsible">
-            <SidebarMenuItem className="flex justify-center">
+        <SidebarMenuItem className="flex justify-center">
+          <SidebarMenuButton
+            asChild
+            className="px-5 flex items-center text-neutral/80 active:text-neutral hover:text-neutral data-[state='open']:hover:text-neutral cursor-pointer data-[active=true]:text-netural"
+            isActive={pathname === ROUTES.DASHBOARD}
+          >
+            <a
+              className="flex flex-1 items-center gap-3 h-fit"
+              onClick={() => navigate(ROUTES.DASHBOARD)}
+            >
+              <div className="flex">
+                <Icon name="LayoutDashboard" size={24} />
+              </div>
+              <span className="text-2xl font-bold">대시보드</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+        <Collapsible defaultOpen className="group/collapsible">
+          <SidebarMenuItem className="flex justify-center">
+            <SidebarMenuButton
+              asChild
+              className="px-5 flex justify-between items-center text-neutral/80 active:text-neutral hover:text-neutral data-[state=open]:hover:text-netural data-[state='open']:hover:text-neutral data-[active=true]:text-netural data-[active=true]:text-neutral"
+              isActive={pathname.startsWith(ROUTES.ORDER)}
+            >
+              <CollapsibleTrigger>
+                <a className="flex flex-1 items-center gap-3 h-fit">
+                  <div className="flex">
+                    <Icon name="ClipboardPen" size={24} />
+                  </div>
+                  <span className="text-2xl font-bold">주문</span>
+                </a>
+                <Icon
+                  className="transition-transform duration-300 group-data-[state=open]/collapsible:rotate-180"
+                  name="ChevronDown"
+                  size={24}
+                  strokeWidth={2}
+                />
+              </CollapsibleTrigger>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+          <CollapsibleContent className="flex flex-col gap-1 py-2">
+            <SidebarMenuItem>
               <SidebarMenuButton
                 asChild
-                className="px-5 flex justify-between items-center text-neutral/80 active:text-neutral hover:text-neutral data-[state='open']:hover:text-neutral"
+                className="pl-10 text-neutral/80 active:text-neutral hover:text-neutral cursor-pointer"
+                onClick={() => navigate(ROUTES.ACTIVE_ORDER)}
               >
-                <CollapsibleTrigger>
-                  <a
-                    className="flex flex-1 items-center gap-3 h-fit"
-                    onClick={() => item.path && navigate(item.path)}
-                  >
-                    <div className="flex">
-                      <Icon name={item.icon} size={24} />
-                    </div>
-                    <span className="text-2xl font-bold">{item.name}</span>
-                  </a>
-                  {item.subMenu && (
-                    <Icon
-                      className="transition-transform duration-300 group-data-[state=open]/collapsible:rotate-180"
-                      name="ChevronDown"
-                      size={24}
-                      strokeWidth={2}
-                    />
-                  )}
-                </CollapsibleTrigger>
+                <a>
+                  <span className="text-2xl">신규 • 진행중</span>
+                </a>
               </SidebarMenuButton>
             </SidebarMenuItem>
-            {item.subMenu && (
-              <CollapsibleContent className="flex flex-col gap-1 py-2">
-                {item.subMenu.map((subItem) => (
-                  <SidebarMenuItem>
-                    <SidebarMenuButton
-                      asChild
-                      className="pl-10 text-neutral/80 active:text-neutral hover:text-neutral cursor-pointer"
-                      onClick={() => navigate(subItem.path)}
-                    >
-                      <a>
-                        <span className="text-2xl">{subItem.name}</span>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </CollapsibleContent>
-            )}
-          </Collapsible>
-        ))}
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                className="pl-10 text-neutral/80 active:text-neutral hover:text-neutral cursor-pointer"
+                onClick={() => navigate(ROUTES.COMPLETED_ORDER)}
+              >
+                <a>
+                  <span className="text-2xl">완료</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </CollapsibleContent>
+        </Collapsible>
       </SidebarMenu>
 
       {/* <SidebarGroup className="p-0">
