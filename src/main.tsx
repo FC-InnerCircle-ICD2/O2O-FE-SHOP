612e5e26a6ae76ba4cc4df0b1d894767ecfedb1e
Suhyeon Jeon <suhyeon.jeon@fig.xyz>
2025-01-12 16:42:14 +0900

    Feat: modal 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import Modal from "@components/Modal"
 import App from "./App"
 import "./styles/global.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Modal />
   </StrictMode>,
 )
